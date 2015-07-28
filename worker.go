b7f85cecbb3bccfa92ef0d484ce4507d1326fab5
Haris Amin <aminharis7@gmail.com>
2015-07-28 16:31:39 -0400

    Adding user importer to main worker.go

--- worker.go
+++ worker.go
@@ -41,6 +41,7 @@ func main() {
 
 	// pull messages from "myqueue" with concurrency of 10
 	workers.Process("instagramediaimportworker", InstagramMediaImportWorker, 10)
+	workers.Process("instagramuserimportworker", InstagramUserImportWorker, 10)
 
 	// stats will be available at http://localhost:8080/stats
 	go workers.StatsServer(5000)
