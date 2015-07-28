24ab92fb9a25d7847be41a16bbbcd0480f252eee
Haris Amin <aminharis7@gmail.com>
2015-07-28 12:20:07 -0400

    just some initial setup stuff, not functional yet

---
+++ Readme.md
@@ -0,0 +1,13 @@
+GRAFF - A Social Graph API
+====
+
+![Gopher](https://blog.golang.org/gopher/gopher.png)![Glimpse](http://a5.mzstatic.com/us/r30/Purple3/v4/54/db/6d/54db6dd9-b62f-bd12-6cd2-9aff80270a28/icon175x175.png) 
+
+
+### Getting Started
+
+1. Install Goop from outside the proejct: `go get github.com/nitrous-io/goop`
+
+2. Run `goop install`
+
+3. Build worker `goop go build worker.go`
---
+++ worker.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/jrallison/go-workers"
+	"os"
+	// "runtime"
+)
+
+func main() {
+	// dbHost := os.Getenv("PG_HOST")
+	// if runtime.NumCPU() > 1 {
+	// 	numOfCpus := runtime.NumCPU() - 1
+	// 	runtime.GOMAXPROCS(numOfCpus)
+	// 	log.Info("Num of CPUs running: ", numOfCpus)
+	// }
+	redisServer := os.Getenv("REDIS_SERVER")
+	redisDB := os.Getenv("REDIS_DB")
+	redisPool := os.Getenv("REDIS_POOL")
+	redisPwd := os.Getenv("REDIS_PWD")
+
+	if (redisServer == "") || (redisDB == "") || (redisPool == "") {
+		// fmt.Println("Please Start Worker with Required Arguments")
+		log.Error("Please Start Worker with Required Arguments")
+		return
+	}
+
+	workers.Configure(map[string]string{
+		// location of redis instance
+		"namespace": "glimpse",
+		"server":    redisServer,
+		// instance of the database
+		"database": redisDB,
+		// redis password
+		"password": redisPwd,
+		// number of connections to keep open with redis
+		"pool": redisPool,
+		// unique process id for this instance of workers (for proper recovery of inprogress jobs on crash)
+		"process": "1",
+	})
+
+	// pull messages from "myqueue" with concurrency of 10
+	workers.Process("neouserimporter", NeoUserImporter, 10)
+	workers.Process("neomediaimporter", NeoMediaImportWorker, 10)
+
+	// stats will be available at http://localhost:8080/stats
+	go workers.StatsServer(5000)
+
+	// Blocks until process is told to exit via unix signal
+	workers.Run()
+}
