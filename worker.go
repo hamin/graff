12283193bab20662660c5d83247cd8126a610fb5
Raquel Hernandez <r@raquel.nyc>
2015-07-30 16:10:47 -0400

    Adding follows import worker to the main package

--- worker.go
+++ worker.go
@@ -42,6 +42,7 @@ func main() {
 	// pull messages from "myqueue" with concurrency of 10
 	workers.Process("instagramediaimportworker", instagram.MediaImportWorker, 10)
 	workers.Process("instagramuserimportworker", instagram.UserImportWorker, 10)
+	workers.Process("followsimportworker", instagram.FollowsImportWorker, 10)
 
 	// stats will be available at http://localhost:8080/stats
 	go workers.StatsServer(5000)
